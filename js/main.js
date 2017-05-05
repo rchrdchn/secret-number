85c5b5847533d0d3699cd6fee7c99300ef086290
Richard Chan <richardchanme@gmail.com>
2017-05-04 22:59:42 -0700

    creating secret guess function and first steps

--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
+var tries = 0;
+
+var $firstGuess = $(".input").value;
+var $secretNumber = Math.floor(Math.random() * $firstGuess) + 1;
+var $userInput = $(".input").value;
+
+function reset() {
+	tries = 0;
+	var $firstGuess = $(".input").value;
+	var $secretNumber = Math.floor(Math.random() * $firstGuess) + 1;
+}
+
+if ($userInput > $secretNumber) {
+	console.log("Your " + $userInput + " is too high");
+	tries++;
+} else if ($userInput < $secretNumber) {
+	console.log("Your " + $userInput + " is too low");
+	tries++;
+} else {
+	console.log("You won in " + tries);
+	reset();
+}
+
+if (tries = 9) {
+	console.log("This is your last try. Good luck!");
+}
+
+if (tries = 10) {
+	console.log("You lost!")
+	reset();
+}
+
+var $button = $(".button");
+
+$button
+
 var timeNow = document.getElementById("time")
 var monthNow = document.getElementById("month")
 
@@ -6,7 +42,8 @@ function getTime() {
 	var hourNow = today.getHours();
 	var minute = today.getMinutes();
 	var second = today.getSeconds();
-	var month = today.getMonth();
+	var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+	var thisMonth = month[today.getMonth()]
 	var day = today.getDay();
 	var year = today.getFullYear();
 
@@ -19,10 +56,11 @@ function getTime() {
 	}	
 	
 	var allTime = hourNow + ":" + minute + ":" + second;
-	var dateNow = month + " " + day + " " + year;
+	var dateNow = thisMonth  + " " + day + ", " + year;
 	
 	timeNow.innerHTML = allTime;
 	monthNow.innerHTML = dateNow;
 }
 
 setInterval(getTime,1000);
+
