2f03ec382e95897b1ef6c3802b13a7c2119775bd
Richard Chan <richardchanme@gmail.com>
2017-05-05 23:22:56 -0700

    user input working

--- js/main.js
+++ js/main.js
@@ -1,78 +1,71 @@
-// number of tries every person begins with
-var tries = 0;
-
-// grab user input value
-var userInput = document.getElementById("input").value;
-// throws a random number for person to guess
-var secretNumber = Math.floor(Math.random() * userInput) + 1;
-
 // function to reset game when player wins or loses
 function reset() {
 	tries = 0;
 
 	var userInput = document.getElementById("input").value;
-	var secretNumber = Math.floor(Math.random() * userInput) + 1;
-}
-
-if (userInput > secretNumber) { // if player input is too high
-	
-	console.log("Your " + userInput + " is too high");
-	tries++;
-	
-	if (tries = 9) {
-	console.log("This is your last try. Good luck!");
-	}
-
-} else if (userInput < secretNumber) { // if player input is too low
-	console.log("Your " + userInput + " is too low");
-	tries++;
-} else { // if player wins, game resets
-	console.log("You won in " + tries);
-	reset();
-}
-
-if (tries = 10) { // if player reaches 10 tries, loses
-
-	console.log("You lost!")
-	reset();
-
+	var secretNumber = Math.floor(Math.random() * 100) + 1;
 }
 
 var button = document.getElementById("button");
 // adds event listener to button
 button.addEventListener("click", function(){
-	console.log("works")
+	// number of tries every person begins with
+	var tries = 0;
+	// grab user input value
+	var userInput = document.getElementById("input").value;
+	// throws a random number for person to guess
+	var secretNumber = Math.floor(Math.random() * 100) + 1;
+
+	if (userInput > secretNumber) { // if player input is too high
+		console.log("Your " + userInput + " is too high");
+		tries++;
+	} else if (userInput < secretNumber) { // if player input is too low
+		console.log("Your " + userInput + " is too low");
+		tries++;
+	} else { // if player wins, game resets
+		console.log("You won in " + tries);
+		reset();
+	}
+	
+	// if player reaches 10 tries, loses
+	// if (tries = 10) { 
+	// 	console.log("You lost!")
+	// 	reset();
+	// }
+
 	// reset whatever is inside input
 
 })
 
-var timeNow = document.getElementById("time")
-var monthNow = document.getElementById("month")
+// set date and time
 
-function getTime() {
-	var today = new Date();
-	var hourNow = today.getHours();
-	var minute = today.getMinutes();
-	var second = today.getSeconds();
-	var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-	var thisMonth = month[today.getMonth()]
-	var day = today.getDay();
-	var year = today.getFullYear();
+// var timeNow = document.getElementById("time")
+// var monthNow = document.getElementById("month")
 
-	if (second < 10) {
-		second = "0" + second;
-	}
+// function getTime() {
+// 	var today = new Date();
+// 	var hourNow = today.getHours();
+// 	var minute = today.getMinutes();
+// 	var second = today.getSeconds();
+// 	var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+// 	var thisMonth = month[today.getMonth()]
+// 	var day = today.getDay();
+// 	var year = today.getFullYear();
 
-	if (minute < 10) {
-		minute = "0" + minute;
-	}	
+// 	if (second < 10) {
+// 		second = "0" + second;
+// 	}
+
+// 	if (minute < 10) {
+// 		minute = "0" + minute;
+// 	}	
 	
-	var allTime = hourNow + ":" + minute + ":" + second;
-	var dateNow = thisMonth  + " " + day + ", " + year;
+// 	var allTime = hourNow + ":" + minute + ":" + second;
+// 	var dateNow = thisMonth  + " " + day + ", " + year;
 	
-	timeNow.innerHTML = allTime;
-	monthNow.innerHTML = dateNow;
-}
+// 	timeNow.innerHTML = allTime;
+// 	monthNow.innerHTML = dateNow;
+// }
 
-setInterval(getTime,1000);
+// setInterval(getTime,1000);
 
